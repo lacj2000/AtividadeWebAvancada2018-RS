1ca84c21425874b60e0d3e016c82434c05643117
lacj2000 <luiz.araujofla@gmail.com>
2018-10-31 11:47:31 -0300

    att-nota10

--- boletim.py
+++ boletim.py
@@ -53,9 +53,26 @@ def cadasDisci(id):
     id1=str(id)
     return redirect('boletim/'+id1+'')
 
-@boletim.route('/alterarDis/<int:ids>')
-def alterarDiscip(ids):
+@boletim.route('/als/<int:id>')
+def als(id):
+    conn = obterConn()
+    search = conn.execute('select * from Disciplinas where id_disciplina=?;', [id])
+    disciplinas = search.fetchone()
+    return render_template('alterarDis.html',disciplina=disciplinas )
 
+@boletim.route('/alterarDis/<int:ids>',methods=['GET','POST'])
+def alterarDiscip(ids):
+    nome = str(request.form['nome'])
+    nota1 = float(request.form['nota1'])
+    nota2 = float(request.form['nota2'])
+    nota3 = float(request.form['nota3'])
+    nota4 = float(request.form['nota4'])
+    ss=obterConn()
+    ss.execute('update Disciplinas set Nome=?,Nota1=?,Nota2=?,Nota3=?,Nota4=? where id_disciplina=?',[nome,nota1,nota2,nota3,nota4,ids])
+    ss.commit()
+    nn=obterConn()
+    idx=nn.execute('select Mat from Disciplinas where id_disciplina=?',[ids])
+    id=idx.fetchone()[0]
     ida=str(id)
     return redirect('boletim/'+ida+'')
 
